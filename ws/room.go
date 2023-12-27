6ac54d3cfafbaed8acb76e3f1e68ce4943d76b55
pecet3 <pecet3107@gmail.com>
2023-12-27 16:40:29 +0100

    init room

---
+++ ws/room.go
@@ -0,0 +1,36 @@
+package ws
+
+type room struct {
+	clients map[*client]bool
+
+	join chan *client
+
+	leave chan *client
+
+	forward chan []byte
+}
+
+func newRoom() *room {
+	return &room{
+		clients: make(map[*client]bool),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		forward: make(chan []byte),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.receive)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.receive <- msg
+			}
+		}
+	}
+}
