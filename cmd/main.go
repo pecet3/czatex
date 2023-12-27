462d024464a61171ac93b6e2fca729d16333209e
pecet3 <pecet3107@gmail.com>
2023-12-28 00:55:41 +0100

    implemented view

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	manager := ws.NewManager()
-
+	index := http.FileServer(http.Dir("view"))
+	http.Handle("/", index)
 	http.Handle("/ws", manager)
 
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
