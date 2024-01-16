38f5135761fed31f016caf8b8e9fdebeaf002e2e
pecet3 <pecet3107@gmail.com>
2024-01-16 15:03:42 +0100

    is it working on docker?

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"fmt"
+
 	"github.com/pecet3/czatex/ws"
 )
 
 func main() {
 	manager := ws.NewManager()
-	index := http.FileServer(http.Dir("view"))
-	log.Println(index)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		log.Println("new conn")
-		fmt.Println("hello")
-	})
-
+	index := http.FileServer(http.Dir("/view/index.html"))
+	http.Handle("/", index)
 	http.Handle("/ws", manager)
 	log.Println("Starting the server")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
