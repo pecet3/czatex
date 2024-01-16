0e64ad1cea80f059fb86c4b8bb24e9b5156223a7
pecet3 <pecet3107@gmail.com>
2024-01-16 14:52:25 +0100

    vvv

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ func main() {
 	index := http.FileServer(http.Dir("view"))
 	log.Println(index)
 	http.HandleFunc("/", func(w http.ResponseWrite, r *http.Request){
-		log.Println(new conn)
+		log.Println("new conn")
 		fmt.Println("hello")
 		w.Write(index)
 	})
+
 	http.Handle("/ws", manager)
 	log.Println("Starting the server")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
