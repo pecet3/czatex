67d676abc9485e206ba79250ba26cc065f997850
pecet3 <pecet3107@gmail.com>
2024-01-16 14:47:07 +0100

    PLZ WORK!!!

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"fmt"
 	"github.com/pecet3/czatex/ws"
 )
 
 func main() {
 	manager := ws.NewManager()
 	index := http.FileServer(http.Dir("view"))
-	http.Handle("/", index)
+	log.Println(index)
+	http.HandleFunc("/", func(w http.ResponseWrite, r *http.Request){
+		log.Println(new conn)
+		fmt.Println("hello")
+		w.Write(index)
+	})
 	http.Handle("/ws", manager)
 	log.Println("Starting the server")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
