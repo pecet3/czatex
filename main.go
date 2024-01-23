e525f18b8ef32e2a3476d137f62475d9ffb326c9
Jakub Pacewicz <115111601+pecet3@users.noreply.github.com>
2024-01-23 10:37:12 +0000

    removedd tls

--- main.go
+++ main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
 	"net/http"
 
 	"github.com/pecet3/czatex/ws"
 )
-
-// func main() {
-// 	manager := ws.NewManager()
-// 	index := http.FileServer(http.Dir("view"))
-// 	http.Handle("/", index)
-// 	http.Handle("/ws", manager)
-
-// 	cert, err := tls.LoadX509KeyPair("fullchain.pem",
-// 		"privkey.pem")
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	config := &tls.Config{
-// 		Certificates: []tls.Certificate{cert},
-// 	}
-
-// 	address := "0.0.0.0:443"
-
-// 	server := &http.Server{
-// 		Addr:      address,
-// 		TLSConfig: config,
-// 	}
-
-// 	log.Println("Server is running: ", address)
-// 	log.Fatal(server.ListenAndServeTLS("", ""))
-// }
 
 func main() {
 	manager := ws.NewManager()
@@ -42,24 +13,12 @@ func main() {
 	http.Handle("/", index)
 	http.Handle("/ws", manager)
 
-	cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/czatex.pecet.it-0001/fullchain.pem",
-		"/etc/letsencrypt/live/czatex.pecet.it-0001/privkey.pem")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	config := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-	}
-
-	address := "0.0.0.0:8443"
+	address := "0.0.0.0:8080" // Zmieniono port na standardowy dla HTTP
 
 	server := &http.Server{
-		Addr:      address,
-		TLSConfig: config,
+		Addr: address,
 	}
 
 	log.Println("Server is running: ", address)
-	log.Fatal(server.ListenAndServeTLS("", ""))
+	log.Fatal(server.ListenAndServe()) // Usunięto argumenty i zmieniono na ListenAndServe
 }
